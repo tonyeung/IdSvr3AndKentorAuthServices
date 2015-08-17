0e0a5aa9eba2fb75b044ee3222423fed200a42f4
robin <rduong@slb.com>
2015-08-17 16:36:16 -0500

    working to hit the api and then get a token

---
+++ api/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IdentityServer3.AccessTokenValidation;
+using Owin;
+using System.Web.Http;
+
+namespace api
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+            // accept access tokens from identityserver and require a scope of 'api1'
+            app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions
+                {
+                    Authority = "https://100.105.80.38:13855",
+                    ValidationMode = ValidationMode.ValidationEndpoint,
+
+                    RequiredScopes = new[] { "api1" }
+                });
+
+            // configure web api
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+
+            // require authentication for all controllers
+            config.Filters.Add(new AuthorizeAttribute());
+
+            app.UseWebApi(config);
+        }
+
+    }
+}
--- client/Program.cs
+++ client/Program.cs
@@ -1,5 +1,4 @@
-﻿using IdentityServer3.Core.Models;
-using IdentityModel.Client;
+﻿using IdentityModel.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,41 +12,26 @@ namespace client
     {
         static void Main(string[] args)
         {
-            var response = GetClientToken().Result;
+            var response = GetClientToken();
+            CallApi(response);
             Console.ReadKey();
         }
-        static async Task<TokenResponse> GetClientToken()
+
+        static TokenResponse GetClientToken()
         {
-            using (var client = new HttpClient())
-            {
+            var client = new TokenClient(
+                "https://100.105.80.38:13855/connect/token",
+                "silicon",
+                "F621F470-9731-4A25-80EF-67A6F7C5F4B8");
 
-                var content = new FormUrlEncodedContent(new[] 
-                {
-                    new KeyValuePair<string, string>("client_id", "silicon"),
-                    new KeyValuePair<string, string>("client_secret", "F621F470-9731-4A25-80EF-67A6F7C5F4B8"),
-                    new KeyValuePair<string, string>("scope", "api1"),
-                    new KeyValuePair<string, string>("grant_type", "client_credentials")
-                });
+            return client.RequestClientCredentialsAsync("api1").Result;
+        }
+        static void CallApi(TokenResponse response)
+        {
+            var client = new HttpClient();
+            client.SetBearerToken(response.AccessToken);
 
-
-                var response = await client.PostAsync("https://100.105.80.38:13855/connect/token", content);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var data = await response.Content.ReadAsStringAsync();
-                    return new TokenResponse()
-                    {
-                        AccessToken = "",
-                        AccessTokenLifetime = 0,
-                        IdentityToken = "",
-                        RefreshToken = ""
-                    };
-                }
-                else
-                {
-                    throw new Exception("ERROR, status: " + response.StatusCode.ToString() + response.Content.ReadAsStringAsync().Result);
-                }
-            }
+            Console.WriteLine(client.GetStringAsync("http://localhost:10084/test").Result);
         }
     }
 
